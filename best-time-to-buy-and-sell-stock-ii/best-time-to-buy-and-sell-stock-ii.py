f81bd860c4e482cef1cdd0b2dac5fdd18fcb01f7
Foysal Ahmed <36992936+Foysal1327@users.noreply.github.com>
2023-10-16 14:18:53 +0600

    Time: 65 ms, Memory: 17.6 MB - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -4,20 +4,7 @@ class Solution:
         length = len(prices)
         for stock in range(0, length-1):
             if(prices[stock + 1] > prices[stock]):
-                profit += prices[stock + 1] - prices[stock]
-            
-#         length = len(prices)
-#         min_price = min(prices)
-#         index_of_min = prices.index(min_price)
-#         # print(length, min_price, index_of_min)
-#         if(index_of_min + 1 == length):
-#             return profit
-#         else:
-#             for stock in range(index_of_min, length-1):
-#                 # print(stock)
-#                 if(prices[stock + 1] > prices[stock]):
-#                     profit += prices[stock + 1] - prices[stock]
-                    
+                profit += prices[stock + 1] - prices[stock]                    
                 
         return profit
 
